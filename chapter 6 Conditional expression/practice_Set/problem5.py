8e74305468ad1212c8debeac545b18135f257031
unknown <vivekkumbhar3310@gmail.com>
2025-10-22 17:46:43 +0530

    example

---
+++ chapter 6 Conditional expression/practice_Set/problem5.py
@@ -0,0 +1,10 @@
+# Write a program which finds out whether a given name is present in a list or not. 
+
+a = ["vivek","harry","rocky", "name"]
+
+name = input("Enter a name ")
+
+if(name in a):
+    print("your name in the list")
+else:
+    print("Your name is not in the list")
