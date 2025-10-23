8aadc5573b7df9aaae362c52b9201123a82c9537
unknown <vivekkumbhar3310@gmail.com>
2025-10-23 10:25:26 +0530

    loops

---
+++ chapter 7 Loops/02_while_loops.py
@@ -0,0 +1,21 @@
+# i =1
+
+# while(i<6):
+#     print(i)
+#     i= i+1
+
+'''
+    Output: 
+    1
+    2
+    3
+    4
+    5
+    '''
+
+    #  Write a program to print 1 to 50 using a while loop.
+
+i=5
+while(i<=50):
+    print(i)
+    i = i+1
---
+++ chapter 7 Loops/03_list_using_while.py
@@ -0,0 +1,9 @@
+# Write a program to print the content of a list using while loops.
+
+a= [1,"vivek", 'rohan', "nikis", False]
+
+i=0
+
+while(i<len(a)):
+        print(a[i])
+        i=i+1
