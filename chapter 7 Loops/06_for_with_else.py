82606376a9701e3084694fa5a5be5608aa5dd9ff
unknown <vivekkumbhar3310@gmail.com>
2025-10-23 10:45:30 +0530

    brak and continue stmt

---
+++ chapter 7 Loops/06_for_with_else.py
@@ -0,0 +1,5 @@
+a  = [2,87,955]
+for i in a:
+    print(i)
+else:
+    print("done") # this is printed when the loop is exhausts
---
+++ chapter 7 Loops/07_break_and_continue.py
@@ -0,0 +1,11 @@
+# for i in range(100):
+#     if(i==34):
+#         break    # exit the loop right now
+#     print(i)
+
+
+for i in range(100):
+    if(i==34):
+        continue    # skip this iteration
+    print(i)
+    
