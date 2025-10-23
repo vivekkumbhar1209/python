f01e57e2d33a072242952c653b7a95fa4f70fd01
unknown <vivekkumbhar3310@gmail.com>
2025-10-23 10:33:31 +0530

    for loops

---
+++ chapter 7 Loops/05_for_loop_iterae.py
@@ -0,0 +1,16 @@
+
+# for loop for list
+a = [1,4,6,234,8]
+for i in a:
+    print(i)
+
+# for loop for tuple
+t=(1,45,78,8)
+for i in t:
+    print(i)
+
+# for loop for string
+a= "vivek"
+for i in a:
+    print(i)
+
